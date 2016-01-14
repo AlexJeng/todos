585c62c370d8b3303f61adf98aa81540cbbe085c
sean1rose <sean1rose@gmail.com>
2016-01-13 22:06:50 -0800

    Initial commit basic todo structure set up

---
+++ todos.js
@@ -0,0 +1,66 @@
+Todos = new Meteor.Collection('todos');
+
+if (Meteor.isClient) {
+  Template.TodosPanel.helpers({
+    items: function(){
+      return Todos.find({}, {
+        sort: {
+          created_at: -1
+        }
+      });
+    },
+    isDoneClass: function(){
+      return this.is_done ? 'done' : '';
+    }
+  });
+
+  Template.TodoItem.helpers({
+    isDoneChecked: function(){
+      // this is the todo item, itself
+      return this.is_done ? 'checked' : '';[]
+    }
+  });
+
+  Template.TodoItem.events({
+    'click [name=is_done]': function(e, tmpl){
+      var id = this._id;
+      // like jquery find method to grab input checkbox element
+      var isDone = tmpl.find('input').checked;
+      Todos.update({_id: id}, {
+        $set: {
+          is_done: isDone
+        }
+      });
+    }
+  });
+
+  Template.CreateTodoItem.events({
+    'submit form': function(e, tmpl){
+      // prevent default so don't post back to server
+      e.preventDefault();
+      // grab inputted value
+      var subject = tmpl.find('input').value
+      Todos.insert({
+        subject: subject, 
+        created_at: new Date,
+        is_done: false
+      });
+      var form = tmpl.find('form');
+      form.reset();
+    }
+  });
+
+  Template.TodosCount.helpers({
+    completedCount: function(){
+      return Todos.find({is_done: true}).count();
+    },
+    totalCount: function(){
+      return Todos.find().count();
+    }
+  });
+
+}
+
+if (Meteor.isServer) {
+
+}
